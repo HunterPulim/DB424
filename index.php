3ac83911cfe7ae9811a62a75fc82fcac10075b29
HunterPulim <67111844@dpu.ac.th>
2024-09-26 11:44:35 +0700

    pagination

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php
 phpinfo();
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+<nav>
+        <ul>
+            <?php if($page > 1): ?>
+            <li>
+                <a href="?page=<?php echo $page-1; ?>&limit=<?php echo $limit; ?>">Previous</a>
+            </li>
+            <?php endif; ?>
+
+            <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+            <li>
+                <a href="?page=<?php echo $i; ?>&limit=<?php echo $limit; ?>"><?php echo $i; ?></a>
+            </li>
+            <?php endfor; ?>
+
+            <?php if($page < $totalPages): ?>
+            <li>
+                <a href="?page=<?php echo $page+1; ?>&limit=<?php echo $limit; ?>">Next</a>
+            </li>
+            <?php endif; ?>
+        </ul>
+    </nav>  
